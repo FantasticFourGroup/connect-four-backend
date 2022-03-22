ad850c34ca55a1878722abf373b66dbeac67db91
Hans Daduya <hansdaduya@yahoo.com>
2022-03-22 08:56:54 +0800

    :passport_control: Change host and port
    Change host and port

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> std::io::Result<()> {
       .service(echo)
       .route("/solve-board", web::post().to(solve_board))
   })
-  .bind(("127.0.0.1", 8080))?
+  .bind(("0.0.0.0", 8088))?
   .run()
   .await
 }
